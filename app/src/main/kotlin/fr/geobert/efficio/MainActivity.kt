8b174c49050cb2b9d7e15c4f224e21ddd4c11107
Geobert Quach <geobert@gmail.com>
2016-01-02 12:22:26 +0000

    add edit button to avoid accidentally trigger edit item

--- app/src/main/kotlin/fr/geobert/efficio/MainActivity.kt
+++ app/src/main/kotlin/fr/geobert/efficio/MainActivity.kt
@@ -1,6 +1,7 @@
 package fr.geobert.efficio
 
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import com.crashlytics.android.Crashlytics
 import io.fabric.sdk.android.Fabric
 
@@ -10,6 +11,7 @@ public class MainActivity : BaseActivity() {
         if (!BuildConfig.DEBUG) Fabric.with(this, Crashlytics());
         setContentView(R.layout.main_activity)
         setTitle(R.string.app_name)
+        titleColor = ContextCompat.getColor(this, android.R.color.primary_text_dark)
         val f = TaskListFragment()
         fragmentManager.beginTransaction().replace(R.id.content, f).commit()
     }
